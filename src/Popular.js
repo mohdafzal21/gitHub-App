4533056ea338b74ecf11e736569dbd6cefa20f47
msfarookhy <msfarookhy@gmail.com>
2018-10-10 15:25:21 +0530

    Leran Funtional Componnet And Prop Type

--- src/Popular.js
+++ src/Popular.js
@@ -1,26 +1,26 @@
-import React from"react";
+import React from "react";
 import PropTypes from 'prop-types';
 
-const SelectedLanguage = (props)=>{
+const SelectedLanguage = (props) => {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
   console.log(props);
-  return(
-    
+  return (
+
 
     <div>
-        <ul className='languages'>
-          {languages.map((lang)=> {
-            return (
-              <li 
-                style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
-                onClick={()=>props.onSelect(lang)}
-                key={lang}>
-                  {lang}
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      <ul className='languages'>
+        {languages.map((lang) => {
+          return (
+            <li
+              style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+              onClick={() => props.onSelect(lang)}
+              key={lang}>
+              {lang}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
 
   )
 }
@@ -32,34 +32,34 @@ class Popular extends React.Component {
     super();
     this.state = {
       selectedLanguage: 'All',
-    };var React = require('react');
+    }; var React = require('react');
 
 
     // this.updateLanguage = this.updateLanguage.bind(this);
   }
-  updateLanguage=(lang)=> {
-    this.setState( ()=> {
+  updateLanguage = (lang) => {
+    this.setState(() => {
       return {
         selectedLanguage: lang,
       }
     });
   }
   render() {
-    
+
 
     return (
       <SelectedLanguage
-      selectedLanguage={this.state.selectedLanguage}
-      onSelect={this.updateLanguage}
+        selectedLanguage={this.state.selectedLanguage}
+        onSelect={this.updateLanguage}
       />
     )
   }
 }
 
 //proptypes for functional component
-SelectedLanguage.propTypes={
-  selectedLanguage : PropTypes.string,
-  onClick :PropTypes.func,
+SelectedLanguage.propTypes = {
+  selectedLanguage: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Popular
